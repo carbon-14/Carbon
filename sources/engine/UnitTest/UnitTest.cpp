984877412725076215439aba5b96406a2a46b65a
carbon14 <carbon14@gmx.com>
2012-09-11 15:57:18 +0200

    fix main proto

--- sources/engine/UnitTest/UnitTest.cpp
+++ sources/engine/UnitTest/UnitTest.cpp
@@ -4,7 +4,7 @@
 #include "Core/HelloWorld.h"
 #include "Core/Platform.h"
 
-int main( int argc, const char* argv[] )
+int main( int argc, char* argv[] )
 {
 	printf( "%s\n", HelloWorld() );
 	printf( "Your platform is : %s\n", GetPlatformName() );
