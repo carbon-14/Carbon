9f9d581f406c9f88a45278a4ae51633a247efc22
carbon14 <carbon14@gmx.com>
2012-10-11 00:31:06 +0200

    Core : - fix hum yep it's static ...

--- sources/engine/Core/Math.h
+++ sources/engine/Core/Math.h
@@ -10,36 +10,36 @@ namespace Core
     class _CoreExport Math
     {
     public:
-        F32 Pi();
-        F32 TwoPi();
-        F32 HalfPi();
-        F32 PosInf();
-        F32 NegInf();
+        static F32 Pi();
+        static F32 TwoPi();
+        static F32 HalfPi();
+        static F32 PosInf();
+        static F32 NegInf();
 
-        S32 IAbs( S32 value );
-        S32 IMin( S32 a, S32 b );
-        S32 IMax( S32 a, S32 b );
-        S32 IClamp( S32 value, S32 min, S32 max );
+        static S32 IAbs( S32 value );
+        static S32 IMin( S32 a, S32 b );
+        static S32 IMax( S32 a, S32 b );
+        static S32 IClamp( S32 value, S32 min, S32 max );
 
-        F32 Abs( F32 value );
-        F32 Min( F32 a, F32 b );
-        F32 Max( F32 a, F32 b );
-        F32 Clamp( F32 value, F32 min, F32 max );
-        F32 Floor( F32 value );
-        F32 Ceil( F32 value );
-        F32 Lerp( F32 left, F32 right, F32 ratio );
+        static F32 Abs( F32 value );
+        static F32 Min( F32 a, F32 b );
+        static F32 Max( F32 a, F32 b );
+        static F32 Clamp( F32 value, F32 min, F32 max );
+        static F32 Floor( F32 value );
+        static F32 Ceil( F32 value );
+        static F32 Lerp( F32 left, F32 right, F32 ratio );
 
-        F32 Sqrt( F32 value );
-        F32 Exp( F32 value );
-        F32 Log( F32 value );
+        static F32 Sqrt( F32 value );
+        static F32 Exp( F32 value );
+        static F32 Log( F32 value );
 
-        F32 Sin( F32 angle );
-        F32 Cos( F32 angle );
-        F32 Tan( F32 angle );
-        F32 ASin( F32 value );
-        F32 ACos( F32 value );
-        F32 ATan( F32 value );
-        void SinCos( F32 angle, F32& sin, F32& cos );
+        static F32 Sin( F32 angle );
+        static F32 Cos( F32 angle );
+        static F32 Tan( F32 angle );
+        static F32 ASin( F32 value );
+        static F32 ACos( F32 value );
+        static F32 ATan( F32 value );
+        static void SinCos( F32 angle, F32& sin, F32& cos );
     };
 }
 
